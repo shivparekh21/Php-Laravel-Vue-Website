2d2e7eca6b983a7b1648d11ad9a3b25a69b67b58
shivparekh21 <64468212+shivparekh21@users.noreply.github.com>
2024-05-27 17:19:51 -0400

    Add files via upload

---
+++ database/migrations/2021_11_19_093939_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->tinyInteger('shopify_id');
+            $table->string('product_title');
+            $table->string('handle');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
